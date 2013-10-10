cb0de5a15f71039f725f7a3e6b056ccd0c96d4e2
yhhc <chenyh@mit.edu>
2013-10-09 23:01:41 -0400

    With David's suggestions on mapping and aggregation
    Much succinct code

--- graph03.r
+++ graph03.r
@@ -5,32 +5,20 @@ setwd("C:/work/R/examples")
 source <- read.csv("c:/work/R/examples/outlook.csv",header=TRUE)
 
 # Create an object with param == 2_Popul
-pop=subset(source,param == "2_Popul")
+pop <- subset(source,param == "2_Popul")
 
-# Create objects which are subsets of pop
-pop_anz=subset(pop,region=="ANZ")
-pop_usa=subset(pop,region=="USA")
-pop_can=subset(pop,region=="CAN")
-pop_jpn=subset(pop,region=="JPN")
-pop_eur=subset(pop,region=="EUR")
+# Create a region-to-group mapping table
+map <- data.frame(
+         region = c("ANZ","USA","CAN","JPN","EUR",  "RUS","BRA","MEX","CHN","IND","ASI",   "AFR","LAM","MES","REA","ROE"),
+         group  = c(rep("Developed",5),              rep("Other G20",6),                    rep("Rest of World",5))  
+                  )
 
-# Drop the column value and replace the region content by "Developed"
-pop_attr=pop_usa[ , -which(names(pop) %in% c("region","value"))]
-pop_attr$region <- "Developed"
+# Merge the object pop with the object map
+pop <- merge(pop, map, by="region", all=TRUE, sort=FALSE)
 
-# Drop all columns except for the column "value"
-pop_anz=pop_anz[ , -which(names(pop_anz) %in% c("param","region","year"))]
-pop_usa=pop_usa[ , -which(names(pop_usa) %in% c("param","region","year"))]
-pop_can=pop_can[ , -which(names(pop_can) %in% c("param","region","year"))]
-pop_jpn=pop_jpn[ , -which(names(pop_jpn) %in% c("param","region","year"))]
-pop_eur=pop_eur[ , -which(names(pop_eur) %in% c("param","region","year"))]
-
-# Sum over these five countries
-value=pop_anz+pop_usa+pop_can+pop_jpn+pop_eur
-
-# Combine pop_attr with value
-pop_developed=cbind(pop_attr,value)
+# Sum up values for a given year within the same group
+pop_group <- aggregate(pop$value, by=list(pop$year,pop$group), FUN=sum)
 
 # Writing the object pop into a CSV file named MyData.csv without the default row labels
-write.csv(pop_developed, file = "c:/work/R/examples/MyData.csv",row.names=FALSE)
+write.csv(pop_group, file = "c:/work/R/examples/MyData.csv",row.names=FALSE)
 
